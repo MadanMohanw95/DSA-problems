654777e0b5723be5c40e82983c0775dd843f74ae
madanmohanw95 <madhanmohanw95@gmail.com>
2025-07-28 20:49:06 -0500

    Add common item detection functions with test cases

---
+++ CommonItem.js
@@ -0,0 +1,43 @@
+var arr1 = ['a', 'b', 'c', 'x']
+var arr2 = ['z', 'y', 'i']
+// should return false
+
+var arr3 = ['a', 'b', 'c', 'x']
+var arr4 = ['z', 'y', 'x']
+// should return true
+
+const commonItem = (arr3, arr4) => {
+    if (!arr3 || !arr4 || arr3.length === 0 || arr4.length === 0)  {
+        return false;
+    }
+
+    const set1 = new Set(arr3);
+
+    for (let char of arr4) {
+        if (set1.has(char)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+const commonItemUsingObject = (arr1, arr2) => {
+    let map = {}
+    for (let item of arr1) {
+        if (!map[item]) {
+            map[item] = true;
+        }
+    }
+
+    for (let item of arr2) {
+        if (map[item]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(commonItem(arr3, arr4))
+console.log(commonItemUsingObject(arr1, arr2))
